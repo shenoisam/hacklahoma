0304b1d052b73fd2e704251b730b6002ed696f39
Sam  Shenoi <samshenoi@d-ip-10-194-74-194.ouguest.nor.ou.edu>
2018-03-03 16:38:54 -0600

    Added get to server

---
+++ hacklahoma/vitechDataExtraction.js
@@ -0,0 +1,14 @@
+var fetch = require('node-fetch')
+var s = function (request, response, next) {
+//Search Participants and retrieve the Participant name, date of birth and gender
+//SELECT pi.name, pi.DOB, pi.sex FROM v_participant pi WHERE pi.state = 'Alaska'
+    url = "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + "&q=state:Alaska" + "&rows=100" + "&fl=name,DOB,sex";
+    fetch(url)
+  		.then(res => res.json())
+  		.then(body => alert(body.response.numFound))
+  		.catch(console.log)
+
+    response.render("VitechGetData.ejs")
+}
+
+module.exports = {s};
