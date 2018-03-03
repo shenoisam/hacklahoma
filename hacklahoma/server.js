b5caf1f97c902b5d761982d0620608589bcf66cd
Sam  Shenoi <samshenoi@d-ip-10-194-74-194.ouguest.nor.ou.edu>
2018-03-03 14:12:03 -0600

    Added the server

---
+++ hacklahoma/server.js
@@ -0,0 +1,43 @@
+// This file contains the instructions for the server.
+// The server is pretty much in charge of setting the content to be displayed using HTTP commands
+
+//Included libraries from the node modules
+var express = require('express')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+
+
+//Create instance of express app
+var app = express()
+
+//Want to serve js and html in ejs (embedded javascript)
+app.set('view engine','ejs')
+
+//Want to send css and images, and other static files
+app.use(express.static('views'))
+
+//View engine - allows us to use static template files like CSS
+app.set('views',__dirname+'/views');
+
+//Give server access to user input
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+//This pretty much just logs stuff out for us.
+app.use(logger('dev'))
+
+//Page turner nav bar responses
+app.get('/',function(request,response){
+  response.render('index.html')
+
+})
+
+
+
+//Set the port that you wish the server to listen to
+var port =  8888
+
+//Listens in on a specific port so that can run
+app.listen(port,function(){
+ console.log("App is running on port"+port)
+  })
