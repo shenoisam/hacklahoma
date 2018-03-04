be50430345fcfbf1c71971e1cb99ed49deccf400
Sam  Shenoi <samshenoi@d-ip-10-194-74-194.ouguest.nor.ou.edu>
2018-03-04 00:17:26 -0600

    Fixed errors in server

--- hacklahoma/nodeServerJSFiles/AWSdata.js
+++ hacklahoma/nodeServerJSFiles/AWSdata.js
@@ -2,30 +2,28 @@
 var mysql = require('mysql');
 var config = require('./config.json');
 
-var pool = mysql.createPool({
-  host     : config.dbhost,
-  user     : config.dbuser,
-  password : config.dbpassword,
-  database : config.dbname
-});
-pool.getConnection(function(err, connection) {
-  // Use the connection
-  connection.query('SELECT * FROM participant;', function (error, results, fields) {
 
-
-    // And done with the connection.
-    connection.release();
-    // Handle error after the release.
-    if (error) {
-      console.log('--------RESULTS ARRAY ONE ---------')
-      throw error;
-    }
-
-    else
-    {
-      console.log('--------RESULTS ARRAY ONE ---------')
-      console.log(results[0].participant_id);
-    }
-    process.exit();
+  var pool = mysql.createPool({
+    host     : config.dbhost,
+    user     : config.dbuser,
+    password : config.dbpassword,
+    database : config.dbname
   });
-});
+  pool.getConnection(function(err, connection) {
+    // Use the connection
+    connection.query('SELECT * FROM participant;', function (error, results, fields) {
+      // And done with the connection.
+      connection.release();
+      // Handle error after the release.
+      if (error) {
+        console.log('--------RESULTS ARRAY ONE ---------')
+        throw error;
+      }
+
+      else
+      {
+        console.log(results[0].participant_id);
+      }
+      process.exit();
+    });
+  });
